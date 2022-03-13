195e9899241546405a10bf842a0576147499e39f
Yidnekachew bantrgs <mekuyidnedawit@gmail.com>
2022-03-13 11:28:37 -0700

    modified

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,15 @@ int main(void)
 	int lch = 122;
 	int ch;
 
-        for (ch = 97; ch <= lch; ch++)
+	for (ch = 97; ch <= lch; ch++)
 	{
-		putchar(ch);
+		putchar (ch);
 
 		if (ch == 122)
 		{
 			ch = 64;
 			lch = 90;
-                }
+		}
 	}
 	printf("\n");
 	return (0);
