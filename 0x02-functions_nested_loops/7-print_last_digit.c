845c0ba132b8a1f95df6772dba2e4e5b1a8a5ac8
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-16 23:37:42 +0300

    task 7-print_last_digit.cis solved

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: is an integer number to be printed it's last digit
+ *
+ * Return: the last digit of an integer on success
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
