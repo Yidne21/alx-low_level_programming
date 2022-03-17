2847794533489a9059906b1010993fc5d5e302e0
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-17 23:09:31 +0300

    solution for task 3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,8 +24,9 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month >= 1 && day >= 60)
+		if (month == 2 && day == 60)
 		{
+			day++;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
