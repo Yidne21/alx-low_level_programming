6466479e56c76e2a9171c5c55f5ef3e9d8b48ee1
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-21 12:08:43 +0300

    solution for task 2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the lengh of.
+ *
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t lengh = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
