c1d8ac15fc2c9e1fca8b079c493fb0aba67b07b4
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-22 18:51:34 +0300

    modified

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	long len = 0, index;
 
 	while (s[index++])
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
+
 	_putchar('\n');
 }
