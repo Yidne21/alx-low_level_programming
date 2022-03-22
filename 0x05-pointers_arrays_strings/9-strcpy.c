fb27e6c23b96dcb65f2761a6f2b5456fdac03a81
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-22 21:14:02 +0300

    solution for task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,11 +7,11 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest,const char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
