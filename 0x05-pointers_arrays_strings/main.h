674092a391ffe2600bd7911b43298218a7732f92
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-22 21:08:36 +0300

    solution for task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,4 +9,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, const char *src);
