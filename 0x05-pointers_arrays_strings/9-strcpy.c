e55edcdc14fb38025763ffadeb7d71b7f37bb84e
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-22 22:19:34 +0300

    modified

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,11 @@ char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
+	while (*(src + index) != '\0')
 	{
-		dest[index] = src[index];
+		dest[index] = *(src + index);
 		index++;
 	}
+	dest[index] = '\0';
 	return (dest);
 }
