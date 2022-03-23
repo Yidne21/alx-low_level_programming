7a5ac70b31758037618eb8c0b69f1a34ea320a35
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-23 10:40:33 +0300

    solution for task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle with height equals to size
+ * @size:is an integer and is the height of the triangle 
+ *
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j <= size - i; j++)
+			_putchar(' ');
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (i == 1)
+		_putchar('\n');
+}
