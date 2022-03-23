644ce5704a6087900383daf6db5ef5151f54fb1f
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-23 08:57:39 +0300

    Modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	char n, c;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar ((j / 10) + '0');
-			_putchar ((j % 10) + '0');
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+			_putchar ('0' + c);
 		}
 		_putchar ('\n');
 	}
