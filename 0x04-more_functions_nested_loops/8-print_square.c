7a107829ce5f0f00f0d91e99696bbd56a44ac0bc
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-23 09:42:04 +0300

    solution for task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - print n by n square with hashtag
+ *
+ * @n: integr and the length of square
+ * Return: Always 0
+ */
+void print_square(int n)
+{
+	int i = 0, j;
+
+	while (i < n && n > 0)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('#');
+		_putchar('\n');
+		i++;
+	}
+	if ( i == 0)
+		_putchar('\n');
+}
