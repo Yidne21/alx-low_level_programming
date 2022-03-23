fa095d8fccd91691b794dc25e417ab3de11302f0
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-23 17:19:22 +0300

    solution for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings using at most
+ *           an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] != '\0'; index++)
+		dest[dest_len + index] = src[index];
+	dest[dest_len + index] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This a c project to understand pointers, arrays and strings from ALX
