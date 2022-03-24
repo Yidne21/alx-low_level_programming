098afeb337693822d7bda2b87bd09ac3a15bd4d0
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-03-24 16:58:08 +0300

    solution for task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,33 @@
  */
 int main(void)
 {
-	unsigned long int i = 3, j, k, next;
+	unsigned long int i = 3, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
-	next = j + k;
-	printf("%lu, %lu, ", j, k);
-	while (i <= 98)
+	printf("%lu, ", j);
+	while (i <= 91)
 	{
-		if (i == 98)
-			printf("%lu\n", next);
-		else
-			printf("%lu, ", next);
-		j = k;
-		k = next;
-		next = j + k;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 		++i;
 	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	i = 92;
+	while (i <= 98)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+		++i;
+	}
+	printf("\n");
 	return (0);
 }
